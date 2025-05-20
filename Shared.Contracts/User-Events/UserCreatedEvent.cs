f413167e24593350418c45ad357b8fc393e3fcdc
EHV DireWolf <jason.spaninks@gmail.com>
2025-05-20 17:06:45 +0200

    Contract changes

---
+++ Shared.Contracts/User-Events/UserCreatedEvent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class UserCreatedEvent {
+    public Guid Id { get; set; }
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+
+    public string CorrelationId { get; set; }
+    public string ReplyTo { get; set; }
+}
---
+++ Shared.Contracts/User-Events/UserCreatedResponse.cs
@@ -0,0 +1,4 @@
+﻿public class UserCreatedResponse {
+    public string CorrelationId { get; set; }
+    public string Status { get; set; }
+}
