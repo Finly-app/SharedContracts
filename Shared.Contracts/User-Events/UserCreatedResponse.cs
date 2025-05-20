feccceb26729b7d84dfff6d3a909c11a2e79254a
EHV DireWolf <jason.spaninks@gmail.com>
2025-05-20 17:56:10 +0200

    Update UserCreatedResponse.cs

--- Shared.Contracts/User-Events/UserCreatedResponse.cs
+++ Shared.Contracts/User-Events/UserCreatedResponse.cs
@@ -1,4 +1,7 @@
-﻿public class UserCreatedResponse {
+﻿using System;
+
+public class UserCreatedResponse {
     public string CorrelationId { get; set; }
+    public Guid UserId { get; set; }
     public string Status { get; set; }
 }
