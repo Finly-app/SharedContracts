41a3c0c7eefd0365b402ce16d6c79aba2857f165
EHV DireWolf <jason.spaninks@gmail.com>
2025-05-20 20:30:57 +0200

    Error message update

--- Shared.Contracts/User-Events/UserCreatedEvent.cs
+++ Shared.Contracts/User-Events/UserCreatedEvent.cs
@@ -4,7 +4,6 @@ public class UserCreatedEvent {
     public string Username { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
-
     public string CorrelationId { get; set; }
     public string ReplyTo { get; set; }
 }
--- Shared.Contracts/User-Events/UserCreatedResponse.cs
+++ Shared.Contracts/User-Events/UserCreatedResponse.cs
@@ -4,4 +4,5 @@ public class UserCreatedResponse {
     public string CorrelationId { get; set; }
     public Guid UserId { get; set; }
     public string Status { get; set; }
+    public string ErrorMessage { get; set; }
 }
