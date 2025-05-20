faeea9ef9516f30d509af8ee9f6264947beeb430
EHV DireWolf <jason.spaninks@gmail.com>
2025-05-20 14:59:23 +0200

    Adjusted User Contract

--- Shared.Contracts/User-Events/UserCreationRequestedEvent.cs
+++ Shared.Contracts/User-Events/UserCreationRequestedEvent.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 
 namespace Shared.Contracts.Events {
-    public class UserCreatedEvent {
-        public Guid Id { get; set; }
+    public class UserCreationRequestedEvent {
+        public Guid TempId { get; set; }
         public string Username { get; set; }
-        public string Password { get; set; }
         public string Email { get; set; }
+        public string Password { get; set; }
     }
+
 }
