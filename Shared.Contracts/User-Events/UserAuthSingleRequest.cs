95df1366343f99403481ead6b20ec137cb3924c7
EHV DireWolf <jason.spaninks@gmail.com>
2025-06-05 16:03:19 +0200

    Added single request

---
+++ Shared.Contracts/User-Events/UserAuthSingleRequest.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+public class UserAuthSingleRequest {
+    public Guid UserId { get; set; }
+    public string CorrelationId { get; set; } = string.Empty;
+    public string ReplyTo { get; set; } = string.Empty;
+}
