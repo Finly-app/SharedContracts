8c687452bb236b190e3cf897d564474907765ae5
EHV DireWolf <jason.spaninks@gmail.com>
2025-06-08 14:00:37 +0200

    Updated update

---
+++ Shared.Contracts/User-Events/UserUpdatedEvent.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class UserUpdatedEvent {
+    public Guid UserId { get; set; }
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string CorrelationId { get; set; }
+    public string ReplyTo { get; set; }
+}
---
+++ Shared.Contracts/User-Events/UserUpdatedResponse.cs
@@ -0,0 +1,5 @@
+﻿public class UserUpdatedResponse {
+    public string CorrelationId { get; set; }
+    public string Status { get; set; }
+    public string ErrorMessage { get; set; }
+}
